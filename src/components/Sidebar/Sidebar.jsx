ab9465d1655acf49407f5937b48ff8c62d28eb73
pong <156582011+PONGSAPAK-BOONSONTHI@users.noreply.github.com>
2024-04-06 03:14:21 +0700

    6/04/24

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -5,10 +5,10 @@ import { assets } from '../../assets/assets'
 const Sidebar = () => {
   return (
     <section className="sidebar">
-      <div>
-        <img className="menu" src={assets.menu_icon} alt="menu_icon" />
+      <div className="top">
+        <button className='menu'><img src={assets.menu_icon} alt="menu_icon" /></button>
 
-        <div className="new-Chat">
+        <div className="new-chat">
           <img src={assets.plus_icon} alt="plus-icon" />
           <p>NewChat</p>
         </div>
@@ -20,6 +20,7 @@ const Sidebar = () => {
             <p>What is react ...</p>
           </div>
         </div>
+        
       </div>
 
       <div className="bottom">
@@ -27,20 +28,17 @@ const Sidebar = () => {
           <img src={assets.question_icon} alt="question_icon" />
           <p>Help</p>
         </div>
-      </div>
 
-      <div className="bottom">
         <div className="bottom-item recent-entry">
           <img src={assets.history_icon} alt="history_icon" />
           <p>Actinity</p>
         </div>
-      </div>
 
-      <div className="bottom">
         <div className="bottom-item recent-entry">
           <img src={assets.setting_icon} alt="" />
           <p>Setting</p>
         </div>
+
       </div>
     </section>
   )
